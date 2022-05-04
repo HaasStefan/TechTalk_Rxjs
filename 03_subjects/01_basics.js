84e972476aab986b7d80fc0e8eddc41bc82659eb
Stefan Haas <stefan.haas@iib.at>
2022-05-04 17:48:21 +0200

    observable unicast example

--- 03_subjects/01_basics.js
+++ 03_subjects/01_basics.js
@@ -1,23 +1,53 @@
-import { Subject, BehaviorSubject } from 'rxjs';
+import { Subject, BehaviorSubject, Observable } from 'rxjs';
  
+
+const observable = new Observable(observer => {
+  observer.next(1);
+  observer.next(2);
+  observer.next(3);
+});
+
+observable.subscribe({
+  next: (v) => console.log(`Observable 1: ${v}`),
+});
+observable.subscribe({
+  next: (v) => console.log(`Observable 2: ${v}`),
+});
+
+// Output:
+// Observable 1: 1
+// Observable 1: 2
+// Observable 1: 3
+// Observable 2: 1
+// Observable 2: 2
+// Observable 2: 3
+console.log("---");
+
+
+
+
 const subject = new Subject();
  
 subject.subscribe({
-  next: (v) => console.log(`observerA: ${v}`),
+  next: (v) => console.log(`Subject 1: ${v}`),
 });
 subject.subscribe({
-  next: (v) => console.log(`observerB: ${v}`),
+  next: (v) => console.log(`Subject 2: ${v}`),
 });
+
  
 subject.next(1);
 subject.next(2);
 
 // Output:
-// observerA: 1
-// observerB: 1
-// observerA: 2
-// observerB: 2
+// Subject 1: 1
+// Subject 2: 1
+// Subject 1: 2
+// Subject 2: 2
 console.log("---");
+
+
+
 
 const behaviorSubject = new BehaviorSubject("initial value");
 
